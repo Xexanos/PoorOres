163f23aaa87b648500739121f8010c269c4211d4
MatheMarcel <Foo6aqu9hau2oohoo6ko>
2014-09-17 08:39:10 +0200

    fixed mod name

--- src/java/net/xexanos/poorores/reference/Reference.java
+++ src/java/net/xexanos/poorores/reference/Reference.java
@@ -1,8 +1,8 @@
 package net.xexanos.poorores.reference;
 
 public class Reference {
-    public static final String MOD_ID = "OilCraft";
-    public static final String MOD_NAME = "OilCraft";
+    public static final String MOD_ID = "PoorOres";
+    public static final String MOD_NAME = "PoorOres";
     public static final String VERSION = "1.7.10-0.0.1";
     public static final String CLIENT_PROXY_CLASS = "net.xexanos.poorores.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.xexanos.poorores.proxy.ServerProxy";
