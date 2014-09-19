22cf66e808cbab497807586032a72e2b8a9fbde2
Xexanos <xexanos@web.de>
2014-09-20 00:05:40 +0200

    localisation of Creative Tab.

--- src/java/net/xexanos/poorores/creativetab/CreativeTabPoorOres.java
+++ src/java/net/xexanos/poorores/creativetab/CreativeTabPoorOres.java
@@ -7,16 +7,10 @@ import net.minecraft.item.Item;
 import net.xexanos.poorores.reference.Reference;
 
 public class CreativeTabPoorOres {
-    public static final CreativeTabs POOR_ORES_TAB = new CreativeTabs(Reference.MOD_ID) {
+    public static final CreativeTabs POOR_ORES_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase()) {
         @Override
         public Item getTabIconItem() {
             return Reference.NUGGETS_LIST.getFirst();
-        }
-
-        @Override
-        @SideOnly(Side.CLIENT)
-        public String getTranslatedTabLabel() {
-            return Reference.MOD_ID;
         }
     };
 }
