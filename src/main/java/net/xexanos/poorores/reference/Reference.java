2daceaba8e513dbe2cb760c489b9405ba8276239
Xexanos <xexanos@web.de>
2015-01-04 12:45:05 +0100

    made change of version easier.

--- src/main/java/net/xexanos/poorores/reference/Reference.java
+++ src/main/java/net/xexanos/poorores/reference/Reference.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 public class Reference {
     public static final String MOD_ID = "PoorOres";
     public static final String MOD_NAME = "PoorOres";
-    public static final String VERSION = "1.7.10-1.4.2";
+    public static final String VERSION = "@VERSION@";
     public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
     public static final String CONFIG_PREFIX = "ores.block_";
 
